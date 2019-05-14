c72d96993213d9e718352f882e9c12190e0b5e00
jvestolas <jv@coverhound.com>
2019-05-13 18:56:42 -0700

    added signup form validation

--- client/src/components/app/index.jsx
+++ client/src/components/app/index.jsx
@@ -8,9 +8,11 @@ class App extends Component {
     super(props);
 
     this.state = {
+      emailValid: false,
       email: '',
       password: '',
       showSignup: true,
+      formSubmitted: false,
     }
   }
 
@@ -20,17 +22,46 @@ class App extends Component {
         handleInputChange={this.updateInputField}
         handleSubmit={this.handleSignupSubmit}
         toggleSignup={this.toggleSignup}
+        emailValid={this.state.emailValid}
+        formSubmitted={this.state.formSubmitted}
+        email={this.state.email}
+        password={this.state.password}
       /> :
       null;
   }
 
-  updateInputField = ({ type, value }) => { this.setState({ [`${type}`]: value }) }
+  updateInputField = ({ type, value }) => {
+    this.setState({
+      [`${type}`]: value,
+      formSubmitted: false,
+    })
+  }
 
   toggleSignup = () => { this.setState({ showSignup: !this.state.showSignup }) }
 
+  validateEmail = () => {
+    const emailRegex = /^\w+@\w+\.[a-zA-Z]{2,3}$/g
+
+    return !!(this.state.email.match(emailRegex))
+  }
+
   handleSignupSubmit = () => {
-    // validate
-    console.log('submitting');
+    this.setState({
+      emailValid: this.validateEmail(),
+      formSubmitted: true,
+    }, () => {
+      if (this.state.emailValid && this.state.password) {
+        this.setState({ showSignup: false });
+      }
+
+      if (!this.state.emailValid) {
+        this.setState({ email: '' });
+      }
+
+      if (!this.state.password.length) {
+        this.setState({ password: '' });
+      }
+    })
   }
 
   render() {
--- client/src/components/sign-up/index.jsx
+++ client/src/components/sign-up/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import styles from './styles';
 
-const SignUp = ({ handleInputChange, handleSubmit, toggleSignup }) => {
+const SignUp = ({ handleInputChange, handleSubmit, toggleSignup, emailValid, formSubmitted, email, password }) => {
   const handleEmailChange = (e) => {
-    handleInputChange({ type: 'email', value: e.target.vallue })
+    handleInputChange({ type: 'email', value: e.target.value })
   }
+
   const handlePasswordChange = (e) => {
-    handleInputChange({ type: 'password', value: e.target.vallue })
+    handleInputChange({ type: 'password', value: e.target.value })
   }
+
+  const showEmailError = cx({ 'inputError': formSubmitted && !emailValid })
+
+  const showPasswordError = cx({ 'inputError': formSubmitted && password.length < 1 })
 
   return(
     <div>
@@ -19,8 +25,19 @@ const SignUp = ({ handleInputChange, handleSubmit, toggleSignup }) => {
             <p onClick={toggleSignup}>&times;</p>
           </div>
           <h3>Sign Up</h3>
-          <input onChange={handleEmailChange} placeholder="Email" />
-          <input onChange={handlePasswordChange} placeholder="Password" />
+          <input
+            value={email}
+            className={showEmailError}
+            onChange={handleEmailChange}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            value={password}
+            className={showPasswordError}
+            onChange={handlePasswordChange}
+            placeholder="Password"
+          />
           <button onClick={handleSubmit}>Continue</button>
         </div>
       </div>
