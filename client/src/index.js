4317a1545340332168518863926c3bcf38673195
jvestolas <jv@coverhound.com>
2019-05-13 19:10:03 -0700

    Add react-router-dom to navigate to /stories   * this is a rollback

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from 'react-dom';
 import App from './components/app';
 
-render(<App />, document.getElementById('learnstart-app'))
+const AppWithRoutes = () => (
+  <Router>
+    <App />
+  </Router>
+)
+
+render(<AppWithRoutes />, document.getElementById('learnstart-app'))
